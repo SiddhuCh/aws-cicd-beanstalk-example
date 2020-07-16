ed25cb70a5869796fe0a0adf457a933b20413d6d
Siddhu Ch <tech.siddhuch@gmail.com>
2020-07-16 18:51:30 +0530

    Project added

---
+++ src/main/java/com/siddhu/aws/aws/AwsCicdBeanstalkExampleApplication.java
@@ -0,0 +1,13 @@
+package com.siddhu.aws.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCicdBeanstalkExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCicdBeanstalkExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siddhu/aws/aws/AwsCicdBeanstalkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddhu.aws.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCicdBeanstalkExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
